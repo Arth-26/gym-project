d8a409872bbff551a40dbe5ca7edb8ff4711cc5d
Arthur Gomes Alves <desenvolvimentonit@gmail.com>
2025-10-15 01:08:41 -0300

    First Get Request

--- src/main/java/com/example/gym_project/GymProjectApplication.java
+++ src/main/java/com/example/gym_project/GymProjectApplication.java
@@ -3,11 +3,10 @@ package com.example.gym_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GymProjectApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(GymProjectApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/gym_project/controller/HelloWorldController.java
+++ src/main/java/com/example/gym_project/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping
     public String sayHello(){
-        return "Hello World";
+        return "Testando novamente";
     }
 }
